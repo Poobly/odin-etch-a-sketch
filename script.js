62e35387f95863f1fe89249e388cff0577224c0e
Tommy <tommychabiras@gmail.com>
2021-12-11 16:31:14 -0500

    added darkening on elements hovered over multiple times, turns black at 10 times

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const resetGrid = document.getElementById("reset-grid");
 const gridAmount = document.getElementById("grid-amount");
 
 
-//document.body.onload = 
 drawGrid();
+
 
 function drawGrid(gridNum = 16) {
     for (let i = 0; i < gridNum; i++) {
         const drawingDivsCon = document.createElement("div");
-        drawingDivsCon.setAttribute("style", "display: flex;")
+        drawingDivsCon.setAttribute("style", "display: flex")
         container.appendChild(drawingDivsCon);
     
         for (let i = 0; i < gridNum; i++) {
             const drawingDivs = document.createElement("div");
-            drawingDivs.addEventListener("mouseover", () => {
+            drawingDivs.addEventListener("mouseover", (e) => {
                 drawingDivs.setAttribute("class", "blue");
+                darkness(e);
             })
             
             drawingDivsCon.appendChild(drawingDivs);
@@ -23,17 +24,27 @@ function drawGrid(gridNum = 16) {
     }
 }
 
+function darkness(e) {
+    let re = /\d+/g;
+    let element = getComputedStyle(e.target).getPropertyValue("background-color");
+    let colour = Number(element.match(re)[2]);
+    colour -= 0.1 * 255;
+    e.target.style.backgroundColor = `rgb( 0, 0, ${colour})`;
+}
+
+
+
 function reset() {
-    const drawingDivs = document.getElementsByClassName("blue");
+    const drawingDivs = document.querySelectorAll("#container > div > div");
     for (let i = drawingDivs.length - 1; i >= 0; i--) {
         drawingDivs[i].removeAttribute("class");
+        drawingDivs[i].style.removeProperty("background-color");
     }
 }
 resetGrid.addEventListener("click", reset);
 
 gridAmount.addEventListener("click", () => {
     let gridNum = parseInt(prompt("How many squares per side? (Under 100)"));
-    console.log(gridNum);
     if (gridNum > 100 || !(Number.isInteger(gridNum))) {
         alert("You entered an invalid value");
         gridNum = 16;
